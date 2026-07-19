15a140fd319e60c39bef969e4b9511b572be69dc
김영준 <dudwns1ms1234@gmail.com>
2026-07-19 23:34:15 +0900

    update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,4 @@
-const API_URL = "https://animal-api-two.vercel.app";
+import App from './App.js';
 
-const $content = document.querySelector('div.content');
-const getData = async () => {
-    let res = await fetch(API_URL);
-    try{
-        if(res){
-            let data = await res.json();
-            $content.innerHTML = data.photos.reduce((p,i) =>{
-                return p += `<img src=${i.url}></img>`;
-            }, "");
-        }
-    } catch(err){
-        console.log(err);
-    }
-}
-
-getData();
+const $app = document.getElementById('app');
+new App($app);
